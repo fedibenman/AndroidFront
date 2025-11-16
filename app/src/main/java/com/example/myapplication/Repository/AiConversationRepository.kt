0e78c88c4e1eeca00cd8fa302c14a0ab45140e98
John Doe <fedibenmansour7@gmail.com>
2025-11-16 19:09:24 +0100

    AiAssitant functionalities

---
+++ app/src/main/java/com/example/myapplication/Repository/AiConversationRepository.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.Repository
+
+class AiConversationRepository {
+}
---
+++ app/src/main/java/com/example/myapplication/viewModel/AiConversationViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.viewModel
+
+class AiConversationViewModel {
+}
