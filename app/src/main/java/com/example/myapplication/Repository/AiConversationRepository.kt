ded35dd790af3596e65fe58f4d421555ddf3a017
chamsbejaoui <mohamedchamseddine.bejaoui@esprit.tn>
2025-12-03 13:45:33 +0100

    feat: Add image analysis and AI conversation repositories.

--- app/src/main/java/com/example/myapplication/Repository/AiConversationRepository.kt
+++ app/src/main/java/com/example/myapplication/Repository/AiConversationRepository.kt
@@ -193,7 +193,7 @@ object ApiClient {
         }
     }
 
-    const val BASE_URL = "http://192.168.109.182:3001/ai-conversations"
+    const val BASE_URL = "http://10.0.2.2:3001/ai-conversations"
 }
 
 
